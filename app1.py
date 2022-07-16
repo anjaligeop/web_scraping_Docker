e6b45eeec78e35992d4afec98ab5891a30f24533
Anjali G <anjali.p@perfios.com>
2022-07-16 12:11:14 +0530

    Added fetch data func

--- app1.py
+++ app1.py
@@ -2,11 +2,22 @@ from flask import Flask, render_template, redirect, url_for, request
 import requests
 import json
 import os
+from bs4 import BeautifulSoup
+import re
 
 app1 = Flask(__name__)
 
-def fetch_data(contents):
+def fetch_data(username,contents):
     data_fetched={}
+    soup = BeautifulSoup(contents,"html.parser")
+    repo = soup.find('a',href=re.compile(r'/anjaligeop\?tab=repositories'))
+    repo_num = repo.find('span').text
+    contributions = soup.find('h2',text=re.compile(r'(\d+)\s+contributions\s+in\s+the\s+last\s+year')).text
+    total_contrib = re.search(r'\d+',contributions).group(0)
+    fullname = soup.find('span',attrs={"itemprop":"name"}).text.strip()
+    if not fullname:
+        fullname = 'NA'
+    data_fetched = {'username':username,'fullName':fullname,'numberOfRepositories':repo_num,'contributions':total_contrib+' last year'}
     return data_fetched
 
 @app1.route('/')
@@ -19,10 +30,10 @@ def login():
         username = request.form['username']        
         response = requests.get('https://github.com/'+username, verify=False)
         filename = os.getcwd()+'/dump.html'
-        # with open(filename,"w") as f:
-        #     f.write(response.text)
-        # f.close   
-       
+        with open(filename,"w") as f:
+            f.write(response.text)
+        f.close
+        data_fetched = fetch_data(username,response.text)
         return render_template('process.html',form_data = request.form) 
     
 
